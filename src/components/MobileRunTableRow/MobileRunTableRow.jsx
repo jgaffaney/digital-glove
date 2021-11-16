5e70e7080b62e2196798df2afecea231100277d8
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-11-15 21:08:20 -0600

    sweetalert confirms delete run

--- src/components/MobileRunTableRow/MobileRunTableRow.jsx
+++ src/components/MobileRunTableRow/MobileRunTableRow.jsx
@@ -1,9 +1,38 @@
 import { TableRow, TableCell, Button } from '@mui/material';
 import { DateTime } from 'luxon';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Stack from '@mui/material/Stack';
+import {useDispatch} from 'react-redux';
 
 function MobileRunTableRow({run}) {
 
-    const dtStart = DateTime.fromISO(run.start_timestamp).toLocaleString(DateTime.DATETIME_SHORT)
+    // define hook functions
+    const dispatch = useDispatch();
+
+    // convert SQL date to readable 
+    const dtStart = DateTime.fromISO(run.start_timestamp).toLocaleString(DateTime.DATETIME_SHORT);
+
+    const MySwal = withReactContent(Swal);
+
+    // delete function with sweetalert confirmation
+    const handleDelete = () => {
+        MySwal.fire({
+            title: <p>This will delete your run and cannot be reversed.  Are you sure?</p>,
+            showConfirmButton: true,
+            showCancelButton: true,
+            icon: 'warning',
+        }).then((result) => {
+            if(result.isConfirmed) {
+                dispatch({type: 'DELETE_RUN', payload: run.id})
+                MySwal.fire('Your run has been deleted')
+            } else {
+                MySwal.fire('Your run has been saved')
+            }
+        })
+    }
     
     return(
         <TableRow 
@@ -12,7 +41,7 @@ function MobileRunTableRow({run}) {
             <TableCell align="right">{run.id}</TableCell>
             <TableCell align="right">{dtStart}</TableCell>
             <TableCell><Button>Edit</Button></TableCell>
-            <TableCell><Button>Delete</Button></TableCell>
+            <TableCell><Button onClick={handleDelete}>Delete</Button></TableCell>
         </TableRow>
     )
 }
