a0c1865de13d1ef18e1b88c8cca3378ce944b568
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-11-23 20:03:17 -0600

    bug fixes reviewtreatment

--- src/components/TreatmentReview/TreatmentReview.jsx
+++ src/components/TreatmentReview/TreatmentReview.jsx
@@ -1,29 +1,31 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { FormControl, TextField, Select, InputLabel, MenuItem, Box, NativeSelect } from '@mui/material';
+import { FormControl, TextField, Select, InputLabel, MenuItem, Box, 
+            NativeSelect, Button, Divider, Dialog, DialogActions, DialogContent,
+            DialogContentText, DialogTitle } from '@mui/material';
 import { DateTime } from 'luxon';
 
 function TreatmentReview() {
 
     const dispatch = useDispatch();
 
-    const treatment = useSelector(store=>store.currentTreatment);
-    const allTreatments = useSelector(store=>store.allTreatments);
+    const treatment = useSelector(store => store.currentTreatment);
+    const allTreatments = useSelector(store => store.allTreatments);
 
     const sortTreatments = (category) => {
         console.log('in sortTreatments');
         let resultArray = [];
-        for(let tx of allTreatments) {
-            if(tx.category == category){
+        for (let tx of allTreatments) {
+            if (tx.category == category) {
                 resultArray.push(tx);
-            } 
+            }
         }
         return resultArray;
     }
 
     const findID = () => {
-        for(let tx of allTreatments){
-            if(tx.procedure == treatment.procedure){
+        for (let tx of allTreatments) {
+            if (tx.procedure == treatment.procedure) {
                 return tx.id
             }
         }
@@ -31,34 +33,48 @@ function TreatmentReview() {
 
     const defaultID = findID();
 
+    const defaultTime = `${DateTime.fromISO(treatment.timestamp).toLocaleString(DateTime.TIME_24_WITH_SECONDS)}`
+
+
     const airway = sortTreatments('airway');
     const chest = sortTreatments('chest');
     const access = sortTreatments('access');
     const medication = sortTreatments('medication');
 
-    const handleChange = () => {
-
-    }
+    console.log('date from treatment: ', DateTime.fromISO(treatment.timestamp).toISODate());
 
     const [newTxDetails, setNewTxDetails] = useState(treatment);
-    // const dtStart = DateTime.fromISO(run.start_timestamp).toLocaleString(DateTime.DATETIME_SHORT);
 
-    useEffect(()=>{
-        dispatch({type: 'FETCH_ALL_TREATMENTS'})
+    const handleTimeChange = (time) => {
+        console.log('time in timechange: ', time);
+        console.log('fromFormat: ', DateTime.fromFormat());
+        const date = DateTime.fromISO(treatment.timestamp).toISODate();
+        // const time = 
+    }
+
+    const handleSubmit = () => {
+        dispatch({type: 'EDIT_TX', payload: treatment })
+    }
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_ALL_TREATMENTS' })
     }, [])
-    console.log('tx review tx: ', treatment.procedure);
-    return(
+    console.log('tx review tx,start_timestamp: ', defaultTime)
+
+    return (
         <Box>
             <FormControl fullWidth>
                 <InputLabel htmlFor='treatment-select'></InputLabel>
                 <NativeSelect
-                inputProps={{
-                    name: 'treatment',
-                    id: 'uncontrolled-native'
-                }}
+                sx={{width: '50%', m:'auto'}}
+                    inputProps={{
+                        name: 'treatment',
+                        id: 'uncontrolled-native'
+                    }}
                     // id='treatment-select'
                     defaultValue={defaultID}
                     label='Treatment'
+                    onChange={(event)=> setNewTxDetails({...newTxDetails, procedure: event.target.value})}
                 >
                     {/* <option aria-label={treatment.procedure} value={treatment.id} /> */}
                     <optgroup label='Airway'>
@@ -66,11 +82,13 @@ function TreatmentReview() {
                             <option key={tx.id} value={tx.id}>{tx.procedure}</option>
                         ))}
                     </optgroup>
+                    <Divider />
                     <optgroup label='Chest'>
                         {chest.map(tx => (
                             <option key={tx.id} value={tx.id}>{tx.procedure}</option>
                         ))}
                     </optgroup>
+                    <Divider />
                     <optgroup label='Access'>
                         {access.map(tx => (
                             <option key={tx.id} value={tx.id}>{tx.procedure}</option>
@@ -82,14 +100,40 @@ function TreatmentReview() {
                         ))}
                     </optgroup>
                 </NativeSelect>
+                <br /> <br />
+                <TextField
+                    id="time"
+                    label="Time"
+                    type="time"
+                    defaultValue={defaultTime}
+                    views={['hours', 'minutes', 'seconds']}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    inputProps={{
+                        step: 60, // 1 min
+                    }}
+                    sx={{ width: 175, m: 'auto' }}
+                    onChange={()=>handleTimeChange(event.target.value)}
+                />
+                <br /><br />
+                <Button
+                    type='submit'
+                    variant='contained'
+                    sx={{m:'auto', width: '60%'}}
+                >
+                    Submit
+                </Button>
             </FormControl>
         </Box>
-        // <FormControl>
-            
-        //     <TextField onChange={(e)=> {setNewTxDetails({...treatment, procedure: e.target.value})}}value={newTxDetails.procedure} id='procedure' label='Procedure' />
-        //     <TextField value={DateTime.fromISO(newTxDetails.timestamp).toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)} id='timestamp' label="Time" />
-        // </FormControl>
     )
+
+    // <FormControl>
+
+    //     <TextField onChange={(e)=> {setNewTxDetails({...treatment, procedure: e.target.value})}}value={newTxDetails.procedure} id='procedure' label='Procedure' />
+    //     <TextField value={DateTime.fromISO(newTxDetails.timestamp).toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)} id='timestamp' label="Time" />
+    // </FormControl>
+
 }
 
 export default TreatmentReview;
